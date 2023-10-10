d597155495fc055aadcbad32599b9f7f14f45c69
Alex Tetervak <tetervak@sheridancollege.ca>
2023-10-10 18:39:09 -0400

    removed unused imports

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -19,12 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.marsphotos.MarsPhotosApplication
 import com.example.marsphotos.data.MarsPhotosRepository
 import com.example.marsphotos.model.MarsPhoto
 import dagger.hilt.android.lifecycle.HiltViewModel
